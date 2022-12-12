b86d9064249c3f7109b61001249b568c2a24aa8e
KalebMolewe23 <molewekaleb33@gmail.com>
2022-12-12 09:29:29 -0800

    update user responsif

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -19,14 +19,74 @@
   <div id="map" style="width: 1200px; height: 600px;"></div>
 </section>
 
-<section class="about container" id="about">
-  <div class="about-img">
-
+<section class="data container" id="data">
+  <!-- box 1 -->
+  <div class="box">
+    <i class='bx bx-user'></i>
+    <h3>Masukan Untuk Kami</h3>
+    <p>Lorem ipsum dolor sitm anet consectetur adipisicing elit. Facilis, aut.</p>
   </div>
-  <div class="about-text">
-
+  <!-- box 2 -->
+  <div class="box">
+    <i class='bx bx-desktop'></i>
+    <h3>Hubungi Kami</h3>
+    <p>Lorem ipsum dolor sitm anet consectetur adipisicing elit. Facilis, aut.</p>
+  </div>
+  <!-- box 3 -->
+  <div class="box">
+    <i class='bx bx-home-alt'></i>
+    <h3>Info Lengkap Manokwari</h3>
+    <p>Lorem ipsum dolor sitm anet consectetur adipisicing elit. Facilis, aut.</p>
   </div>
 </section>
+
+<section class="about container" id="about">
+  <div class="about-img">
+    <img src="<?= base_url('assets/user/malaria.jpg') ?>" alt="">
+  </div>
+  <div class="about-text">
+    <span>About Us</span>
+    <h2>Informasi Malaria Pada Manokwari<br> Papua Barat</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto debitis ducimus</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto debitis ducimus</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto debitis ducimus</p>
+    <p>Lorem ipsum dolor, sit amet consctetur adipisicing elit. Voluptatem, soluta.</p>
+    <a href="#" class="btn">Selengkapnya</a>
+  </div>
+</section>
+
+<section class="footer">
+  <div class="footer-container container">
+    <h2>Malaria</h2>
+      <div class="footer-box">
+        <h3>Link</h3>
+        <a href="#">Home</a>
+        <a href="#">Pemetaan</a>
+        <a href="#">Data Malaria</a>
+        <a href="#">Tentang Kami</a>
+      </div>
+      <div class="footer-box">
+        <h3>Alamat</h3>
+        <a href="#">Manokwari</a>
+        <a href="#">Papua Barat</a>
+        <a href="#">Indonesia</a>
+      </div>
+      <div class="footer-box">
+        <h3>Kontak</h3>
+        <a href="#">nomor anda</a>
+        <a href="#">emailkamu@gmail.com</a>
+        <div class="social">
+          <a href="#"><i class="bx bxl-facebook"></i></a>
+          <a href="#"><i class="bx bxl-twitter"></i></a>
+          <a href="#"><i class="bx bxl-instagram"></i></a>
+        </div>
+      </div>
+  </div>
+</section>
+
+<div class="copyright">
+  <p>&#169; Malaria Papua Barat</p>
+</div>
 
 <div id="myModal_2019" class="modal">
 <div class="modal-content">
